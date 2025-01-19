5366d1a55daaf68be603fc6bf79e781b9543d11e
Laerte Chaves <54726369+Laio47@users.noreply.github.com>
2025-01-19 15:21:58 -0300

    Update Tarefa2-ControleGPIO_Por_PortaSerial.c

--- Tarefa2-ControleGPIO_Por_PortaSerial.c
+++ Tarefa2-ControleGPIO_Por_PortaSerial.c
@@ -1,7 +1,47 @@
+// Definições dos pinos
+#define LED_GREEN 11
+#define LED_BLUE 12
+#define LED_RED 13
+#define BUZZER 21
+
+int main() {
+    // Inicializa UART
+    stdio_init_all();
+
+    // Configura os GPIOs como saída
+    gpio_init(LED_GREEN);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_put(LED_GREEN, false);
+
+    gpio_init(LED_BLUE);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+    gpio_put(LED_BLUE, false);
+
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+    gpio_put(LED_RED, false);
+
+    gpio_init(BUZZER);
+    gpio_set_dir(BUZZER, GPIO_OUT);
+    gpio_put(BUZZER, false);
+
+    printf("Sistema iniciado. Aguardando comandos via UART...\n");
+
+
+
+    while (true) {
+        int ch = getchar_timeout_us(1000000); // Aguarda entrada por 1 segundo
+        if (ch != PICO_ERROR_TIMEOUT) {
+            processar_comando((char)ch);
+        }
+    }
+    return 0;
+}
+
+
+
+
 // Processa o comando recebido via UART
-// 
-
-
 void processar_comando(char comando) {
     switch (comando) {
         case 'G': // Liga o LED verde
@@ -33,4 +73,3 @@ void processar_comando(char comando) {
             break;
     }
 }
-
